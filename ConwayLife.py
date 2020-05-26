53e7e9afe304e3414a3d11094111b371cb0f014a
justin-henley <justin.a.henley@gmail.com>
2020-05-26 11:44:30 +0800

    Structure for ConwayLife.py

---
+++ ConwayLife.py
@@ -0,0 +1,42 @@
+import time, random, copy
+
+# Global constants for character representation and board size
+LIVE_CHAR = '#'
+DEAD_CHAR = ' '
+WIDTH = 20
+HEIGHT = 20
+
+# Generates a new board of size WIDTH*HEIGHT
+# In: Height and Width
+# Out: Returns a 2D list representing a game board
+def generateBoard():
+    # Stub
+    return empty
+
+# Prints the board
+# In: a filled game board
+def printBoard(gameBoard):
+    # stub
+    print('printBoard incomplete')
+
+# Checks the board and generates the next turn's board
+# In: current game board
+# Out: returns the next frame of the game
+def checkBoard(gameBoard):
+    # stub
+    return empty
+
+# Decides whether a cell will be live or dead in the next frame
+# In: the current board, and the coordinates of the cell in wuestion
+# Out: Returns LIVE_CHAR or DEAD_CHAR
+def checkCell(board, xpos, ypos):
+    # stub
+    return DEAD_CELL
+
+# Main program
+def main():
+    #stub
+    print('Hello, world!')
+
+# Begins the program
+main()
